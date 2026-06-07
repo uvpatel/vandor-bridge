ecd4abda756bfae9104dc47806f822abbdd65eb9
uvpatel <uvpatel7271@gmail.com>
2026-06-07 11:22:25 +0530

    Fix the auth

--- src/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { sentinelClient } from "@better-auth/infra/client";
-
-export const authClient = createAuthClient({
-  // ... your existing config
-  plugins: [
-    // ... other plugins
-    sentinelClient()
-  ]
-})
--- src/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import { dash } from "@better-auth/infra";
-
-export const auth = betterAuth({
-  // ... your existing config
-  plugins: [
-    // ... other plugins
-    dash()
-  ]
-})
